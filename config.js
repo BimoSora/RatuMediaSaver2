738f6784e697fc9422caa826a285271c81916aa4
BimoSora <bimosora3@gmail.com>
2022-01-15 13:07:47 +0700

    first commit

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
     //Membuat klien baru. Anda dapat menaruh opsi di file tgsnake.config.js.
-    apiHash: '4014387adad45b7b504cfaa532715244',
+    apiHash: '',
     // Dapatkan dari my.telegram.org
-    apiId: '14402011',
+    apiId: '',
     // Jika anda ingin login dengan akun anda, hapus ini dan buat sesi terlebih dahulu. lalu tempel sesi anda di bidang session.
-    botToken: '5014010959:AAGSwHQZBcCf-E95LOSfitkxinQPqJYrIf4',
+    botToken: '',
     // Isi dengan USER ID akun yang akan di jadikan admin.
-    ADMIN: '2036455164'
+    ADMIN: ''
 }
