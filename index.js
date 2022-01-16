404720d33cd2dea867a5cfdbf0e8b6f696a194ff
BimoSora <bimosora3@gmail.com>
2022-01-16 14:35:21 +0700

    first commit

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function fromid(ctx){
 // bot.generateSession() // aktifkan ini untuk menghasilkan sesi dan nonaktifkan bot.run().
 
 bot.hears(new RegExp(`^[${bot.prefix}](url) (https?:\/\/.*)`,""),async (ctx) => {
-  if(ctx.from.id == Number(config.ADMIN) || ctx.from.id == Number(config.ADMIN1) || ctx.from.id == Number(config.ADMIN2) || ctx.from.id == Number(config.ADMIN3) || ctx.from.id == Number(config.ADMIN4)){
+  if(ctx.from.id == Number(process.env.ADMIN) || ctx.from.id == Number(process.env.ADMIN1) || ctx.from.id == Number(process.env.ADMIN2) || ctx.from.id == Number(process.env.ADMIN3) || ctx.from.id == Number(process.env.ADMIN4)){
     const url = ctx.text.replace('/url', '').trim();
     const regex = /youtube.com|youtu.be/g;
     const found = url.match(regex);
@@ -54,7 +54,8 @@ bot.hears(new RegExp(`^[${bot.prefix}](url) (https?:\/\/.*)`,""),async (ctx) => 
       if(ctx.from.username == undefined){
         mention = ctx.from.first_name
       }
-      try{
+
+  try{
         youtubedl(url, {
           format: `bestvideo[height<=${dq}]+bestaudio/best[height<=${dq}]`,
           dumpSingleJson: true,
@@ -64,54 +65,57 @@ bot.hears(new RegExp(`^[${bot.prefix}](url) (https?:\/\/.*)`,""),async (ctx) => 
           preferFreeFormats: true,
           youtubeSkipDashManifest: true,
         }).then(async output => {
-            const filename = `${output.title}.mp4`
-            await ctx.telegram.sendMessage(ctx.chat.id,`Upload start!`)
-            const buffer = []
-            const stream = got.stream(output.requested_formats[0].url)
-            stream
-            .on('error', () => ctx.telegram.sendMessage(ctx.chat.id, 'An error has occurred'))
-            .on('progress', p => console.log(p))
-            .on('data', chunk => buffer.push(chunk))
-            .on('end', async () => {
-              await ctx.telegram.sendDocument(ctx.chat.id,Buffer.concat(buffer),{
-                fileName : filename
-              })
-              await ctx.telegram.sendMessage(ctx.chat.id,filename)
-              await ctx.telegram.sendMessage(ctx.chat.id,`Upload successful`)
+          const filename = `${output.title}.mp4`
+          await ctx.telegram.sendMessage(ctx.chat.id,`Upload start!`)
+          const buffer = []
+          const stream = got.stream(output.requested_formats[0].url)
+          stream
+          .on('error', () => ctx.telegram.sendMessage(ctx.chat.id, 'An error has occurred'))
+          .on('progress', p => console.log(p))
+          .on('data', chunk => buffer.push(chunk))
+          .on('end', async () => {
+            await ctx.telegram.sendDocument(ctx.chat.id,Buffer.concat(buffer),{
+              fileName : filename
             })
+            await ctx.telegram.sendMessage(ctx.chat.id,filename)
+            await ctx.telegram.sendMessage(ctx.chat.id,`Upload successful`)
           })
-        }catch (error) {
-          console.error(error);
-          ctx.telegram.sendMessage(ctx.chat.id,"***Error occurred, Make sure your sent a correct URL***",{ replyToMsgId: message_id , parse_mode: 'Markdown'})
+        })
+      }catch (error) {
+        console.error(error);
+        ctx.telegram.sendMessage(ctx.chat.id,"***Error occurred, Make sure your sent a correct URL***",{ replyToMsgId: message_id , parse_mode: 'Markdown'})
       }
 
     }else{
 
-      //if (!url) return ctx.telegram.sendMessage(chatId, 'No valid url found')
       const filename = url.split('/').pop()
 
       var regex3 = /\.[A-Za-z0-9]+$/gm
       var doctext3 = filename.replace(regex3, '');
       var doctext4 = filename.replace(regex3, 'null');
 
-      if(doctext3 == doctext4){
-        await ctx.telegram.sendMessage(ctx.chat.id,`Exstension not found`)
-      }else{
-        await ctx.telegram.sendMessage(ctx.chat.id,`Upload start!`)
-        const buffer = []
-        const stream = got.stream(url)
-        stream
-        .on('error', () => ctx.telegram.sendMessage(ctx.chat.id, 'An error has occurred'))
-        .on('progress', p => console.log(p))
-        .on('data', chunk => buffer.push(chunk))
-        .on('end', async () => {
-          await ctx.telegram.sendDocument(ctx.chat.id,Buffer.concat(buffer),{
-            fileName : filename
+  try{
+        if(doctext3 == doctext4){
+          await ctx.telegram.sendMessage(ctx.chat.id,`Exstension not found`)
+        }else{
+          await ctx.telegram.sendMessage(ctx.chat.id,`Upload start!`)
+          const buffer = []
+          const stream = got.stream(url)
+          stream
+          .on('error', () => ctx.telegram.sendMessage(ctx.chat.id, 'An error has occurred'))
+          .on('progress', p => console.log(p))
+          .on('data', chunk => buffer.push(chunk))
+          .on('end', async () => {
+            await ctx.telegram.sendDocument(ctx.chat.id,Buffer.concat(buffer),{
+              fileName : filename
+            })
+            await ctx.telegram.sendMessage(ctx.chat.id,filename)
+            await ctx.telegram.sendMessage(ctx.chat.id,`Upload successful`)
           })
-          await ctx.telegram.sendMessage(ctx.chat.id,filename)
-          await ctx.telegram.sendMessage(ctx.chat.id,`Upload successful`)
-        })
-
+        }
+      }catch (error) {
+        console.error(error);
+        ctx.telegram.sendMessage(ctx.chat.id,"***Error occurred, Make sure your sent a correct URL***",{ replyToMsgId: message_id , parse_mode: 'Markdown'})
       }
     }
   }
